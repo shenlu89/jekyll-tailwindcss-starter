6c2ff195462d8fa06a631f32af13785d099d5696
shenlu89 <shenlu89dev@gmail.com>
2021-08-25 13:08:12 +0800

    build(style): add tailwindcss, postcss and its config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["**/*.{html,md}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
